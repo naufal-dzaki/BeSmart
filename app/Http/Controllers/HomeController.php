eec2932f7ddf3ca6091ebc15b5958a7e8e0812a5
Naufal-dzaki <muhammadnaufaldzaki261@gmail.com>
2022-01-31 12:12:54 +0700

    controller home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,10 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Subject;
+use App\Models\Post;
 
 class HomeController extends Controller
 {
     public function index(){
-        return view('home.index');
+        return view('home.index', [
+            'title' => 'Home',
+            'subjects' => Subject::all()
+        ]);
+    }
+
+    public function show(Subject $Subject){
+        return view('contents.materi', [
+            'title' => 'Home | Mata Pelajaran',
+            'posts' => Post::latest()
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,7 @@ Route::get('/', function () {
 // });
 
 Route::get('/home', [HomeController::class, 'index']);
+Route::get('/home/{subject:slug}', [HomeController::class, 'show']);
 
 Route::get('/tugas', function(){
     return view('contents.tugas', [
