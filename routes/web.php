080128be04e73bb1da1f038a263ba6729f2d24c2
muhBilal <mb336450@gmail.com>
2022-02-12 12:12:04 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -20,60 +20,18 @@ Route::get('/', function () {
         'title' => ' '
     ]);
 });
-
-// Route::get('/loginsa', function () {
-//     return view('login.index');
-// });
-
-Route::get('/home', [HomeController::class, 'index'])->name('home');
-Route::get('/home/{subject:slug}', [HomeController::class, 'show']);
-
-Route::get('/subject/detail', function(){
-    return view('home.subject.detail', [
-        'title' => ' | Subject'
-    ]);
-});
-
-Route::get('/tugas', function(){
-    return view('contents.tugas', [
-        'title' => '| Tugas'
-    ]);
-});
-
-Route::get('/absen', function(){
-    return view('contents.absen', [
-        'title' => '| Absen'
-    ]);
-});
-
-Route::get('/biodata', function(){
-    return view('contents.biodata', [
-        'title' => '| Biodata'
-    ]);
-});
-
-Route::get('/materi', function(){
-    return view('contents.materi', [
-        'title' => ' | Materi'
-    ]);
-});
-
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
-
 require __DIR__.'/auth.php';
-
-Route::post('/logout', [LogoutController::class, 'logout']);
-
-Route::get('/coba', function(){
-    return view('dashboard.index');
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/dashboard', function () {return view('dashboard');})->name('dashboard');
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+    Route::get('/home/{subject:slug}', [HomeController::class, 'show']);
+    Route::get('/subject/detail', function(){return view('home.subject.detail', ['title' => ' | Subject']);});
+    Route::get('/tugas', function(){return view('contents.tugas', ['title' => '| Tugas']);});
+    Route::get('/absen', function(){return view('contents.absen', ['title' => '| Absen']);});
+    Route::get('/biodata', function(){return view('contents.biodata', ['title' => '| Biodata']);});
+    Route::get('/materi', function(){return view('contents.materi', ['title' => ' | Materi']);});
+    Route::get('/coba', function(){return view('dashboard.index');});
+    Route::post('/logout', [LogoutController::class, 'logout']);
+    // absen
+    Route::get('/absen', function(){return view('presensi.absen', ['title' => ' | Absen']);});
 });
-
-// Route::get('/besmart', function(){
-//     return view('layouts.master');
-// });
-
-
-
