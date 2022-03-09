691a21adef1097aad13a95b4129cd65e1d40706a
Naufal-dzaki <muhammadnaufaldzaki261@gmail.com>
2022-03-09 16:20:39 +0700

    merapikan seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,126 +22,13 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        //user
-        User::Create([
-            'id' => 1,
-            'name' => 'naufal',
-            'grade_id' => '2',
-            'email' => 'naufal@gmail.com',
-            'password' => bcrypt('password')
-        ]);
-        User::Create([
-            'id' => 2,
-            'name' => 'bilal',
-            'grade_id' => '1',
-            'email' => 'bilal@gmail.com',
-            'password' => bcrypt('password')
-        ]);
+        $this->call(UserSeeder::class);
 
-        // user detail
-        Biodata::Create([
-            'user_id' => 1,
-            'slug' => 'muhammad-naufal',
-            'tgl_lahir' => 'Surabaya, 9 Mei 2003',
-            'nisn' => '988123098',
-            'no_hp' => '08154567984',
-            'jenis_kelamin' => 'laki-laki',
-            'agama' => 'islam',
-            'nama_ayah' => 'Ini nama ayahnya',
-            'nama_ibu' => 'ini nama ibunya'
-        ]);
-        Biodata::Create([
-            'user_id' => 2,
-            'slug' => 'muhammad-bilal',
-            'tgl_lahir' => 'Surabaya, 9 Mei 2004',
-            'nisn' => '988123098',
-            'no_hp' => '0815456798129',
-            'jenis_kelamin' => 'laki-laki',
-            'agama' => 'islam',
-            'nama_ayah' => 'Ini nama ayahnya',
-            'nama_ibu' => 'ini nama ibunya'
-        ]);
+        $this->call(GradeSeeder::class);
 
-        $faker = \Faker\Factory::create();
+        $this->call(SubjectSeeder::class);
 
-        $total = User::count();
-
-        for ($i = $total+1; $i <= $total+10; $i++) {
-            $user = User::Create([
-                'id' => $i,
-                'name' => $faker->name,
-                'grade_id' => '2',
-                'email' => $faker->email,
-                'password' => bcrypt('password')
-            ]);
-
-            Biodata::Create([
-                'user_id' => $user->id,
-                'slug' => Str::slug($user->name),
-                'image' => 'user-profille.png',
-                'tgl_lahir' => $faker->date($format = 'Y-m-d', $max = 'now'),
-                'nisn' => rand(100000000, 999999999),
-                'no_hp' => $faker->phoneNumber,
-                'jenis_kelamin' =>  $faker->randomElements(['male', 'female'])[0],
-                'agama' => $faker->randomElements(['islam', 'kristen', 'hindu', 'budha'])[0],
-                'nama_ayah' => $faker->name,
-                'nama_ibu' => $faker->name
-            ]);
-        }
-
-        //kelas
-        Grade::create([
-            'grade' => 'X',
-            'slug' => 'x'
-        ]);
-
-        Grade::create([
-            'grade' => 'XI',
-            'slug' => 'xi'
-        ]);
-
-        Grade::create([
-            'grade' => 'XII',
-            'slug' => 'xii'
-        ]);
-
-        //post
-        Post::create([
-            'judul' => 'ini judul',
-            'slug' => 'ini-judul',
-            'bab' => '1',
-            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex, repellat rem sed inventore a omnis eius vero quaerat porro dolores expedita corporis, totam harum quidem nihil numquam accusamus beatae atque illo quo. Facilis illum perferendis accusantium hic, nesciunt corporis nulla amet obcaecati inventore maxime, corrupti dolore nemo, sed impedit repellat itaque unde incidunt quae asperiores! Similique et error quod adipisci at autem dolore iure reprehenderit? Consequuntur quibusdam quaerat ipsam fugit incidunt eligendi ratione quos ducimus aliquam consequatur. Nesciunt architecto earum alias dolorum quasi, illum enim tenetur sequi repellat beatae hic ab, dicta omnis tempora magni quod eum vitae eos! Ab, id voluptas nulla explicabo magni tempora cupiditate autem, molestiae praesentium voluptatem quod consequuntur quas et! Quo totam ducimus illo optio!',
-            'tipe' => '0',
-            'subject_id' => '2',
-            'user_id' => '1'
-        ]);
-        Post::create([
-            'judul' => 'ini adalah mapel pertama',
-            'slug' => 'ini-adalah-mapel-pertama',
-            'bab' => '1',
-            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex, repellat rem sed inventore a omnis eius vero quaerat porro dolores expedita corporis, totam harum quidem nihil numquam accusamus beatae atque illo quo. Facilis illum perferendis accusantium hic, nesciunt corporis nulla amet obcaecati inventore maxime, corrupti dolore nemo, sed impedit repellat itaque unde incidunt quae asperiores! Similique et error quod adipisci at autem dolore iure reprehenderit? Consequuntur quibusdam quaerat ipsam fugit incidunt eligendi ratione quos ducimus aliquam consequatur. Nesciunt architecto earum alias dolorum quasi, illum enim tenetur sequi repellat beatae hic ab, dicta omnis tempora magni quod eum vitae eos! Ab, id voluptas nulla explicabo magni tempora cupiditate autem, molestiae praesentium voluptatem quod consequuntur quas et! Quo totam ducimus illo optio!',
-            'tipe' => '0',
-            'subject_id' => '1',
-            'user_id' => '2'
-        ]);
-        Post::create([
-            'judul' => 'ini adalah mapel kedua',
-            'slug' => 'ini-adalah-mapel-kedua',
-            'bab' => '1',
-            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex, repellat rem sed inventore a omnis eius vero quaerat porro dolores expedita corporis, totam harum quidem nihil numquam accusamus beatae atque illo quo. Facilis illum perferendis accusantium hic, nesciunt corporis nulla amet obcaecati inventore maxime, corrupti dolore nemo, sed impedit repellat itaque unde incidunt quae asperiores! Similique et error quod adipisci at autem dolore iure reprehenderit? Consequuntur quibusdam quaerat ipsam fugit incidunt eligendi ratione quos ducimus aliquam consequatur. Nesciunt architecto earum alias dolorum quasi, illum enim tenetur sequi repellat beatae hic ab, dicta omnis tempora magni quod eum vitae eos! Ab, id voluptas nulla explicabo magni tempora cupiditate autem, molestiae praesentium voluptatem quod consequuntur quas et! Quo totam ducimus illo optio!',
-            'tipe' => '1',
-            'subject_id' => '1',
-            'user_id' => '2'
-        ]);
-        Post::create([
-            'judul' => 'ini adalah mapel tugas',
-            'slug' => 'ini-adalah-mapel-tugas',
-            'bab' => '5',
-            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex, repellat rem sed inventore a omnis eius vero quaerat porro dolores expedita corporis, totam harum quidem nihil numquam accusamus beatae atque illo quo. Facilis illum perferendis accusantium hic, nesciunt corporis nulla amet obcaecati inventore maxime, corrupti dolore nemo, sed impedit repellat itaque unde incidunt quae asperiores! Similique et error quod adipisci at autem dolore iure reprehenderit? Consequuntur quibusdam quaerat ipsam fugit incidunt eligendi ratione quos ducimus aliquam consequatur. Nesciunt architecto earum alias dolorum quasi, illum enim tenetur sequi repellat beatae hic ab, dicta omnis tempora magni quod eum vitae eos! Ab, id voluptas nulla explicabo magni tempora cupiditate autem, molestiae praesentium voluptatem quod consequuntur quas et! Quo totam ducimus illo optio!',
-            'tipe' => '1',
-            'subject_id' => '3',
-            'user_id' => '2'
-        ]);
+        $this->call(PostSeeder::class);
 
         Motivation::create([
             'judul' => 'Ini Adalah Judul Yang Pertama',
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Biodata;
+use Illuminate\Support\Str;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+                //user
+                User::Create([
+                    'id' => 1,
+                    'name' => 'naufal',
+                    'grade_id' => '2',
+                    'email' => 'naufal@gmail.com',
+                    'password' => bcrypt('password')
+                ]);
+                User::Create([
+                    'id' => 2,
+                    'name' => 'bilal',
+                    'grade_id' => '1',
+                    'email' => 'bilal@gmail.com',
+                    'password' => bcrypt('password')
+                ]);
+
+                // user detail
+                Biodata::Create([
+                    'user_id' => 1,
+                    'slug' => 'muhammad-naufal',
+                    'tgl_lahir' => 'Surabaya, 9 Mei 2003',
+                    'nisn' => '988123098',
+                    'no_hp' => '08154567984',
+                    'jenis_kelamin' => 'laki-laki',
+                    'agama' => 'islam',
+                    'nama_ayah' => 'Ini nama ayahnya',
+                    'nama_ibu' => 'ini nama ibunya'
+                ]);
+                Biodata::Create([
+                    'user_id' => 2,
+                    'slug' => 'muhammad-bilal',
+                    'tgl_lahir' => 'Surabaya, 9 Mei 2004',
+                    'nisn' => '988123098',
+                    'no_hp' => '0815456798129',
+                    'jenis_kelamin' => 'laki-laki',
+                    'agama' => 'islam',
+                    'nama_ayah' => 'Ini nama ayahnya',
+                    'nama_ibu' => 'ini nama ibunya'
+                ]);
+
+                $faker = \Faker\Factory::create();
+
+                $total = User::count();
+
+                for ($i = $total+1; $i <= $total+10; $i++) {
+                    $user = User::Create([
+                        'id' => $i,
+                        'name' => $faker->name,
+                        'grade_id' => '2',
+                        'email' => $faker->email,
+                        'password' => bcrypt('password')
+                    ]);
+
+                    Biodata::Create([
+                        'user_id' => $user->id,
+                        'slug' => Str::slug($user->name),
+                        'image' => 'user-profille.png',
+                        'tgl_lahir' => $faker->date($format = 'Y-m-d', $max = 'now'),
+                        'nisn' => rand(100000000, 999999999),
+                        'no_hp' => $faker->phoneNumber,
+                        'jenis_kelamin' =>  $faker->randomElements(['male', 'female'])[0],
+                        'agama' => $faker->randomElements(['islam', 'kristen', 'hindu', 'budha'])[0],
+                        'nama_ayah' => $faker->name,
+                        'nama_ibu' => $faker->name
+                    ]);
+                }
+
+    }
+}
